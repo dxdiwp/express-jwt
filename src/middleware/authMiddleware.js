1197e24b1c887f43b373c792cc8cda67309ced07
dodi wahyu <dodiwahyu28@gmail.com>
2025-02-05 21:34:33 +0700

    Adding authMiddleware.js

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({
+      errors: "Access denied!, token required",
+    });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({
+        errors: "Invalid or expired token",
+      });
+    }
+    req.user = user;
+    next();
+  });
+};
+
+export default authMiddleware;
