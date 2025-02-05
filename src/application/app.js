257f71e9acd8600deedaebdca2780fbe8ec65d9a
dodi wahyu <dodiwahyu28@gmail.com>
2025-02-05 21:33:23 +0700

    Adding route.js

---
+++ src/application/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+import dotenv from "dotenv";
+import { router } from "../route/route.js";
+
+dotenv.config();
+export const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(router);
