3f90987a5c8a3212e01490d6b9f99bb63d8d65bc
dodi wahyu <dodiwahyu28@gmail.com>
2025-02-06 23:37:52 +0700

    Update app.js

--- src/application/app.js
+++ src/application/app.js
@@ -6,9 +6,5 @@ dotenv.config();
 export const app = express();
 
 app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
+app.use(express.urlencoded({extended: true}));
 app.use(router);
